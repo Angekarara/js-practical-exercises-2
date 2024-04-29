73d1384a7a3d10306a91b733a750c44b933879f7
Angekarara <mbabange2020@gmail.com>
2024-04-29 17:41:53 +0200

    classes exercise

---
+++ classes.js
@@ -0,0 +1,33 @@
+/*
+ * Programming Quiz: Building Classes and Subclasses (2-3)
+ */
+
+class Vehicle {
+  constructor(color = "blue", wheels = 4, horn = "beep beep") {
+    this.color = color;
+    this.wheels = wheels;
+    this.horn = horn;
+  }
+
+  honkHorn() {
+    console.log(this.horn);
+    console.log(this.wheels);
+  }
+}
+class Bicycle extends Vehicle {
+  constructor(color = "blue", wheels = 2, horn = "honk honk") {
+    super(color, wheels, horn);
+  }
+  honkHorn() {
+    super.honkHorn();
+    console.log(this.horn);
+    console.log(this.wheels);
+  }
+}
+
+// your code goes here
+
+const myVehicle = new Vehicle();
+myVehicle.honkHorn(); // beep beep
+const myBike = new Bicycle();
+myBike.honkHorn(); // honk honk
