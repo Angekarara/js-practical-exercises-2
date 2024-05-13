c24077b38e9a2d6bebadb68f67a692f4a59f0bb5
Angekarara <mbabange2020@gmail.com>
2024-05-13 18:49:38 +0200

    iterator

---
+++ iterator.js
@@ -0,0 +1,6 @@
+const array = [1, 4, 5, 9][Symbol.iterator]();
+console.log(array.next());
+console.log(array.next());
+console.log(array.next());
+console.log(array.next());
+console.log(array.next());
